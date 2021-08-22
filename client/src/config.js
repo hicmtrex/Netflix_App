5315c9dba5da8d8947a2f35b56dde04bc0d6d44b
hicm <hicmrex@gmail.com>
2021-08-22 02:13:24 +0100

    api/

--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
  const axiosInstance = axios.create({
-     baseURL : "https://netflix-hicm.herokuapp.com/" 
+     baseURL : "https://netflix-hicm.herokuapp.com/api" 
 })
 
 
